a5e9c19a7bbed1cbf1462a0a986b89f3510a4fd7
tmdakm <tmdakm@163.com>
2026-02-04 02:07:12 +0800

    ver 1.7.0 - 支持1.21.11。

--- src/main/java/win/huangyu/cntiertagger/modmenu/CntierTaggerMenuIntegration.java
+++ src/main/java/win/huangyu/cntiertagger/modmenu/CntierTaggerMenuIntegration.java
@@ -3,14 +3,19 @@ package win.huangyu.cntiertagger.modmenu;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.text.Text;
 import win.huangyu.cntiertagger.ModConfig;
 
 @Environment(EnvType.CLIENT)
 public class CntierTaggerMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
+        return parent -> ConfigBuilder.create()
+                .setParentScreen(parent)
+                .setTitle(Text.translatable("text.autoconfig.cntiertagger.title"))
+                .build();
     }
 }

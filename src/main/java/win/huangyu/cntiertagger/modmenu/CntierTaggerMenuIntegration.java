b5f5caba4d4f2e62d0b315c70ac02b71082b4335
tmdakm <tmdakm@163.com>
2026-02-04 16:59:29 +0800

    ver 1.7.1 - 修复配置界面打不开的问题

--- src/main/java/win/huangyu/cntiertagger/modmenu/CntierTaggerMenuIntegration.java
+++ src/main/java/win/huangyu/cntiertagger/modmenu/CntierTaggerMenuIntegration.java
@@ -2,20 +2,16 @@ package win.huangyu.cntiertagger.modmenu;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.autoconfig.AutoConfigClient;
+import me.shedaniel.autoconfig.ConfigData;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.text.Text;
 import win.huangyu.cntiertagger.ModConfig;
 
 @Environment(EnvType.CLIENT)
 public class CntierTaggerMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> ConfigBuilder.create()
-                .setParentScreen(parent)
-                .setTitle(Text.translatable("text.autoconfig.cntiertagger.title"))
-                .build();
+        return parent -> AutoConfigClient.getConfigScreen(ModConfig.class, parent).get();
     }
 }
